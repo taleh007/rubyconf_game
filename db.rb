f5c2cdd4afc4a4045923062b318df0405bde6d7c
Taleh Zaliyev <z.taleh007@ya.ru>
2018-04-16 18:34:06 +0300

    add snippets to db

--- db.rb
+++ db.rb
@@ -1,8 +1,15 @@
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
+require_relative 'mega_db'
+
 DB_WATIR = Sequel.sqlite(database: 'watir.db')
 DB_YOUTUBE = Sequel.sqlite(database: 'youtube.db')
+DB_ERRORS = Sequel.sqlite(database: 'errors.db')
+
+_mega = MegaDB.new
+_e = DB_ERRORS[:errors]
+_t = DB_WATIR[:tasks]
 
 def save_titles_in_file(name)
   tasks = DB_WATIR[:tasks]
@@ -13,6 +20,12 @@ def save_titles_in_file(name)
   end
 end
 
+def tis(s,t,y,r,l,a)
+  tasks = DB_WATIR[:tasks]
+  tasks.insert({speaker: s, title: t, year: y, right: r, left: l, answer: a})
+  tasks.insert({speaker: s, title: t, year: y, left: r, right: l, answer: a})  
+end 
+
 def save_titles_with_speaker_in_file(name)
   tasks = DB_WATIR[:tasks]
   File.open(name, 'w+') do |f|
@@ -22,6 +35,16 @@ def save_titles_with_speaker_in_file(name)
   end
 end
 
+def title_errors_arr
+  errors = DB_ERRORS[:errors]
+  errors.map(:title).each_with_object({}){|v, h| h[v]||=0; h[v]+=1}.sort_by{|x| -x.last}
+end
+
+def title_tasks_arr
+  tasks = DB_WATIR[:tasks]
+  tasks.map(:title).each_with_object({}){|v, h| h[v]||=0; h[v]+=1}.sort_by{|x| -x.last}
+end
+
 binding.pry
 
-put "Exit"
+puts "Exit"
