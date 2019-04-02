5abde3adfd93d0dad918b7d2778737fa4a098a72
Taleh Zaliyev <z.taleh007@ya.ru>
2019-04-02 21:41:18 +0300

    [2019] add gem loader

---
+++ 2019/libs/gem_loader.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
+DB = Sequel.sqlite(database: 'db/main.db', max_connections: 10, logger: Logger.new('log/db.log'))
+
+gem_names = DB[:tasks].all.flat_map { |r| [r[:left], r[:right]] }.uniq.sort
+
+Dir.chdir('./gemdata') do
+  dirs = Dir.entries('.').each_with_object({}) do |name, memo|
+    memo[name] = true unless name =~ %r/\.gem$/
+  end
+
+  gem_names.first(100).each do |gem_name|
+    if dirs[gem_name]
+      puts "GEM '#{gem_name}' already exist'"
+      next
+    end
+
+    system("gem fetch -q #{gem_name}")
+
+    gem_file = Dir.entries('.').select {|x| x =~ /^#{gem_name}.*gem$/ }.first
+    FileUtils.mv(gem_file, "#{gem_name}.gem")
+    system("gem unpack #{gem_name}.gem")
+  end
+end
